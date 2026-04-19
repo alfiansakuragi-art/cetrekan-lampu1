6c0adedbfb72a63a1f4c490588c9509f1a04d5ee
LEFIN <alfiansakuragi@gmail.com>
2026-04-19 09:34:44 +0800

    delete ds.store

--- saklar.js
+++ saklar.js
@@ -1,14 +1,27 @@
-function saklar(command) {
+function saklar(command, type) {
 
     let lampu1 = document.getElementById("lampu1");
     let lampu2 = document.getElementById("lampu2");
     let lampu3 = document.getElementById("lampu3");
 
     if (command == "on") {
-        lampu1.src = 'asset/images/on.gif';
+        if (type == "lampu1") {
+            lampu1.src = 'asset/images/on.gif';
+        }
+        if (type == "lampu2") {
+            lampu2.src = 'asset/images/on.gif';
+        }
+        if (type == "lampu3") {
+            lampu3.src = 'asset/images/on.gif';
+        }
+
     } else if (command == "off") {
-        console.log("off");
-        lampu1.src = 'asset/images/off.gif';
+        if (type == "lampu1")
+            lampu1.src = 'asset/images/off.gif';
+        if (type == "lampu2")
+            lampu2.src = 'asset/images/off.gif';
+        if (type == "lampu3")
+            lampu3.src = 'asset/images/off.gif';
     }
     return lampu1;
 
